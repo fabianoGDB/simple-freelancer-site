c31228a2c78004fb81486936822f087d7fbd8701
fabianoGDB <doctorfabi.f@gmail.com>
2025-05-05 23:42:15 -0300

    A003: creting entities, enums and exceptions /n creating and adding the infrastructure layer to the project

---
+++ DevFreela.Core/Entities/ProjectComment.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevFreela.Core.Entities
+{
+    public class ProjectComment : BaseEntity
+    {
+        public ProjectComment(string content, int idProject, int idUser, DateTime createdAt)
+        {
+            Content = content;
+            IdProject = idProject;
+            IdUser = idUser;
+            CreatedAt = DateTime.Now;
+        }
+
+        public string Content { get; private set; }
+        public int IdProject { get; private set; }
+        public int IdUser { get; private set; }
+
+        public DateTime CreatedAt { get; private set; }
+    }
+}
---
+++ DevFreela.Core/Entities/User.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevFreela.Core.Entities
+{
+    public class User : BaseEntity
+    {
+        public User(string name, string email, DateTime birthDate)
+        {
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+
+
+            CreatedAt = DateTime.Now;
+
+            Skills = new List<UserSkill>();
+            OwnedProjects = new List<Project>();
+            FreelanceProjects = new List<Project>();
+
+            Active = true;
+        }
+
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public DateTime BirthDate { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public bool Active { get; set; }
+        public List<UserSkill> Skills { get; private set; }
+        public List<Project> OwnedProjects { get; private set; }
+        public List<Project> FreelanceProjects { get; private set; }
+
+    }
+
+}
